67561e215288fed04f6aa08c8b26b5c7b47ba8c1
Ben <hacketbe@mail.gvsu.edu>
2025-10-20 15:11:59 -0400

    Don't break please!

---
+++ getMeOut.py
@@ -0,0 +1,28 @@
+A="Carl Weezer"
+B="Jimmy Neutron"
+C="Sheen Estevez"
+D="Cindy Vortex"
+E="Libby Folfax"
+F="Hugh Neutron"
+G="Nick Dean"
+H="Bolbi Stroganovsky"
+I="Professor Calamitous"
+J="Beautiful Gorgeous"
+K="Earl Neutron"
+L=55
+M=3.14
+N=True 
+
+
+
+if (L > M):
+    print(B)
+if (N == True):
+    print(A)
+
+
+def BillyFunction():
+    A="billy bob"
+    print(A)
+
+BillyFunction()
